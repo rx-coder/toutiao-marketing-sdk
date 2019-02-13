c1822faae5259ac2d5b902a9948fb37273d9ac11
zhang5207892 <2269025237@qq.com>
2019-02-13 10:04:51 +0800

    #张文宗 新增测试-获取创意列表

---
+++ test/AdvertisingOriginality/CreativeGet.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 获取创意列表
+ * User: zhangwenzong
+ * Date: 2019/2/13
+ * Time: 10:00
+ */
+require __DIR__ . '/../../index.php';
+require __DIR__ . '/../config.php';
+$auth = new \core\Profile\ToutiaoAuth(APPID, SECRET);
+$client = $auth->makeClient(TOKEN);
+$req = new \AdvertisingOriginality\V1\CreativeGet();
+$req->setAdvertiserId(ADVERTISER_ID);
+var_dump($client->excute($req)->getBody());
